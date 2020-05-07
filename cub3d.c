4f71e4726580bcbaa478566f9d2bfccea9141969
Angeli Kramp <akramp@f0r2s10.codam.nl>
2020-05-07 16:10:51 +0200

    red screen of death

--- cub3d.c
+++ cub3d.c
@@ -6,18 +6,51 @@
 /*   By: akramp <marvin@codam.nl>                     +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/06 14:24:05 by akramp        #+#    #+#                 */
-/*   Updated: 2020/05/06 15:35:52 by akramp        ########   odam.nl         */
+/*   Updated: 2020/05/07 16:01:01 by akramp        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 
+typedef struct	s_data {
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int		main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	int 	x;
+	int 	y;
+	t_data	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,&img.endian);
+	y = 0;
+	while (y < 1080)
+	{
+		x = 0;
+		while (x < 1920)
+		{
+			my_mlx_pixel_put(&img, x, y, 0x00FF0000);
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
