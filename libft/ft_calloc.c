c9a9ed127805bb79b334c89be5b94a03cae2a77e
angeli-sk <solange@kalea.nl>
2020-08-27 21:28:22 +0200

    libft gnl

---
+++ libft/ft_calloc.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_calloc.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: akramp <marvin@codam.nl>                     +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/06 15:48:24 by akramp         #+#    #+#                */
+/*   Updated: 2019/11/17 19:27:47 by akramp        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*s;
+
+	s = malloc(size * count);
+	if (s == 0)
+		return (0);
+	ft_bzero(s, count);
+	return (s);
+}
